c69e3ed2d2170500f2f939ea02b69cb3660e0970
rptleebest <rptlee12@gmail.com>
2026-05-29 22:49:08 +0900

    Create quick_check_presets.py

---
+++ data/quick_check_presets.py
@@ -0,0 +1,178 @@
+QUICK_CHECK_PRESETS = {
+    "목-팔": [
+        {
+            "name": "C5-C6 목 신경뿌리병증 패턴",
+            "description": "감각신경활동전위 보존, 척추주위근 이상, 같은 신경뿌리 분절의 여러 말초신경 지배근 이상이 함께 있는 경우입니다.",
+            "codes": [
+                "snap_preserved",
+                "paraspinal_abnormal",
+                "same_root_multiple_nerves",
+                "c5_c6_muscle_pattern",
+                "fib_psw",
+            ],
+        },
+        {
+            "name": "C7 목 신경뿌리병증 패턴",
+            "description": "척추주위근 이상과 C7 관련 근육군 이상이 중심인 패턴입니다.",
+            "codes": [
+                "snap_preserved",
+                "paraspinal_abnormal",
+                "same_root_multiple_nerves",
+                "c7_muscle_pattern",
+                "fib_psw",
+            ],
+        },
+        {
+            "name": "노신경병증 패턴",
+            "description": "손목처짐, 노신경 감각/운동 이상, 말초 단일신경 분포 침범에 맞는 패턴입니다.",
+            "codes": [
+                "radial_neuropathy_pattern",
+                "snap_reduced",
+                "cmap_reduced",
+                "single_peripheral_nerve",
+                "paraspinal_normal",
+                "fib_psw",
+            ],
+        },
+    ],
+    "손-손목": [
+        {
+            "name": "손목굴증후군 패턴",
+            "description": "정중신경 손목 부위 포착을 시사하는 전형적 패턴입니다.",
+            "codes": [
+                "median_wrist_pattern",
+                "sensory_latency_delayed",
+                "motor_latency_delayed",
+            ],
+        },
+        {
+            "name": "팔꿈치 부위 자신경병증 패턴",
+            "description": "자신경 분포 감각 이상과 손 자체근 침범이 중심인 패턴입니다.",
+            "codes": [
+                "ulnar_elbow_pattern",
+                "sensory_latency_delayed",
+                "motor_latency_delayed",
+                "single_peripheral_nerve",
+                "paraspinal_normal",
+            ],
+        },
+        {
+            "name": "C8-T1 또는 손 자체근 중심 패턴",
+            "description": "손 자체근 이상이 중심이지만 말초신경 포착과 신경뿌리병증 감별이 필요한 경우입니다.",
+            "codes": [
+                "c8_t1_muscle_pattern",
+                "fib_psw",
+            ],
+        },
+    ],
+    "허리-다리": [
+        {
+            "name": "L5 허리 신경뿌리병증 패턴",
+            "description": "발처짐, L5 분절 근육군 이상, 척추주위근 이상, 감각신경 보존이 특징입니다.",
+            "codes": [
+                "l5_pattern",
+                "snap_preserved",
+                "paraspinal_abnormal",
+                "same_root_multiple_nerves",
+                "fib_psw",
+            ],
+        },
+        {
+            "name": "S1 허리 신경뿌리병증 패턴",
+            "description": "발바닥굽힘 약화, H반사 지연/소실, S1 관련 근육 이상에 맞는 패턴입니다.",
+            "codes": [
+                "s1_pattern",
+                "snap_preserved",
+                "paraspinal_abnormal",
+                "h_reflex_delayed_absent",
+                "fib_psw",
+            ],
+        },
+        {
+            "name": "허리엉치신경얼기병증 패턴",
+            "description": "감각신경활동전위 감소, 여러 말초신경 분포 침범, 척추주위근 보존이 핵심입니다.",
+            "codes": [
+                "lumbosacral_plexus_pattern",
+                "multiple_peripheral_nerves",
+                "snap_reduced",
+                "paraspinal_normal",
+                "fib_psw",
+                "no_muap",
+            ],
+        },
+    ],
+    "발-발목": [
+        {
+            "name": "온종아리신경병증 패턴",
+            "description": "발처짐과 발등 감각저하, 단일 말초신경 분포 침범, 척추주위근 보존에 맞는 패턴입니다.",
+            "codes": [
+                "peroneal_neuropathy_pattern",
+                "snap_reduced",
+                "cmap_reduced",
+                "single_peripheral_nerve",
+                "paraspinal_normal",
+                "fib_psw",
+            ],
+        },
+        {
+            "name": "L5 허리 신경뿌리병증 감별 패턴",
+            "description": "발처짐이 있지만 감각신경 보존과 척추주위근 이상이 동반되는 경우입니다.",
+            "codes": [
+                "l5_pattern",
+                "snap_preserved",
+                "paraspinal_abnormal",
+                "same_root_multiple_nerves",
+                "fib_psw",
+            ],
+        },
+    ],
+    "얼굴": [
+        {
+            "name": "눈깜빡반사 들신경 경로 이상 패턴",
+            "description": "삼차신경 들신경 경로 이상을 시사하는 눈깜빡반사 패턴입니다.",
+            "codes": [
+                "blink_afferent_pattern",
+            ],
+        },
+        {
+            "name": "눈깜빡반사 날신경 경로 이상 패턴",
+            "description": "얼굴신경 날신경 경로 이상을 시사하는 패턴입니다.",
+            "codes": [
+                "blink_efferent_pattern",
+            ],
+        },
+    ],
+    "전신/양측": [
+        {
+            "name": "축삭성 다발신경병증 패턴",
+            "description": "양측 원위부 대칭성 감각/운동 진폭 감소가 중심인 패턴입니다.",
+            "codes": [
+                "distal_symmetric_pattern",
+                "distal_symmetric_sensory",
+                "distal_symmetric_motor",
+                "snap_reduced",
+                "cmap_reduced",
+            ],
+        },
+        {
+            "name": "말이집탈락성 다발신경병증 패턴",
+            "description": "전도속도 저하, 잠복기 지연, F파 이상과 근위·원위부 약화가 함께 있는 패턴입니다.",
+            "codes": [
+                "proximal_and_distal_weakness",
+                "conduction_velocity_slow",
+                "motor_latency_delayed",
+                "f_wave_delayed_absent",
+                "distal_symmetric_pattern",
+            ],
+        },
+    ],
+    "뇌졸중 후 경직": [
+        {
+            "name": "뇌졸중 후 경직 증가 패턴",
+            "description": "H반사 항진 또는 H/M 비율 증가와 중추성 경직 증가에 맞는 패턴입니다.",
+            "codes": [
+                "h_reflex_hyperactive",
+            ],
+        },
+    ],
+}
