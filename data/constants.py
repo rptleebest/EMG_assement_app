d7988ca3483c5a74ca8ef0c16967a37a99b0f672
rptleebest <rptlee12@gmail.com>
2026-05-29 16:45:14 +0900

    Update constants.py

--- data/constants.py
+++ data/constants.py
@@ -1,5 +1,16 @@
 MODE_CASE = "사례 학습"
-MODE_DIRECT = "검사 정보 입력 학습"
+MODE_ABNORMAL_CHECK = "이상 소견 체크 학습"
+MODE_DIRECT = "수치 직접 입력 학습"
 
 SEX_OPTIONS = ["미선택", "남", "여"]
 SIDE_OPTIONS = ["미선택", "좌", "우", "양측"]
+
+BODY_REGION_OPTIONS = [
+    "목-팔",
+    "손-손목",
+    "허리-다리",
+    "발-발목",
+    "얼굴",
+    "전신/양측",
+    "뇌졸중 후 경직",
+]
