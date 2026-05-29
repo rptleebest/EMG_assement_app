c285dbd06ae9e098fe9dc718d294e1f6827284ba
rptleebest <rptlee12@gmail.com>
2026-05-29 16:45:46 +0900

    Create glossary.py

---
+++ data/glossary.py
@@ -0,0 +1,42 @@
+GLOSSARY = {
+    "SNAP": {
+        "first": "감각신경활동전위(Sensory Nerve Action Potential, SNAP)",
+        "short": "감각신경활동전위",
+    },
+    "CMAP": {
+        "first": "복합근육활동전위(Compound Muscle Action Potential, CMAP)",
+        "short": "복합근육활동전위",
+    },
+    "FIB": {
+        "first": "섬유자발전위(Fibrillation Potential)",
+        "short": "섬유자발전위",
+    },
+    "PSW": {
+        "first": "양성예파(Positive Sharp Wave)",
+        "short": "양성예파",
+    },
+    "MUAP": {
+        "first": "운동단위전위(Motor Unit Action Potential, MUAP)",
+        "short": "운동단위전위",
+    },
+    "RADICULOPATHY": {
+        "first": "신경뿌리병증(Radiculopathy)",
+        "short": "신경뿌리병증",
+    },
+    "PLEXOPATHY": {
+        "first": "신경얼기병증(Plexopathy)",
+        "short": "신경얼기병증",
+    },
+    "POLYNEUROPATHY": {
+        "first": "다발신경병증(Polyneuropathy)",
+        "short": "다발신경병증",
+    },
+    "F_WAVE": {
+        "first": "F파(F-wave)",
+        "short": "F파",
+    },
+    "H_REFLEX": {
+        "first": "H반사(H-reflex)",
+        "short": "H반사",
+    },
+}
